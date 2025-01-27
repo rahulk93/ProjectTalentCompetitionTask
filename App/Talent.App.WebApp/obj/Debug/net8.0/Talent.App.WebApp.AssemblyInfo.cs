d8e361e71805524a9773b12d5cb02f96d157ab75
Rahul Khatri <53160725+rahulk93@users.noreply.github.com>
2025-01-28 02:06:32 +1300

    Git changes occured due to updating NuGet packages and installation npm packages!

--- App/Talent.App.WebApp/obj/Debug/net8.0/Talent.App.WebApp.AssemblyInfo.cs
+++ App/Talent.App.WebApp/obj/Debug/net8.0/Talent.App.WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Talent.App.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18b66f880efe9dfb6e623098cffaf8d33ca6f26d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9be35f26c183bcae002042f5310b022dfaf87de7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Talent.App.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Talent.App.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
